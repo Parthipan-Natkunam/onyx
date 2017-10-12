34bafb77172efacc085178f12b1c94d67d12c9a8
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2017-10-13 00:52:46 +0530

    add css and js minification tasks to the build system

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,16 +14,47 @@ module.exports = function(grunt) {
         }
       }
     },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist',
+          ext: '.min.css'
+        }]
+      }
+    },
+    uglify:{
+      options: {
+        mangle:{
+          toplevel: true
+        },
+        compress: true,
+        beautify: false
+      },
+      target:{
+        files:{
+          "dist/onyx.min.js":["js/onyx.js"]
+        }
+      }
+    },
     watch: {
       styles: {
-        files: ['less/**/*.less'], // which files to watch
-        tasks: ['less'],
+        files: ['less/**/*.less'],
+        tasks: ['less','cssmin'],
+        options: {
+          nospawn: true
+        }
+      },
+      scripts:{
+        files: ['js/**/*.js'],
+        tasks: ['uglify'],
         options: {
           nospawn: true
         }
       }
     }
   });
-
-  grunt.registerTask('default', ['less', 'watch']);
+  grunt.registerTask('default', ['less', 'cssmin', 'uglify', 'watch']);
 };
